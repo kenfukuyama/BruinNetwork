fe2c1d539e397dfd7a89da830eb37ed1038db279
Ken Fukuyama <kenfukuyama0112@gmail.com>
2022-09-09 12:01:26 -0700

    build config

---
+++ client/build/static/js/searchBar.js
@@ -0,0 +1,9 @@
+// alert("serach bar acitve")
+$(document).ready(function(){
+    $(".live-search-box").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $(".live-search-list").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });

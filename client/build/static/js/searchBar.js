a8c0be547670a5aae3d3aca682e8d187e0c7ba9f
Ken Fukuyama <46236452+kenfukuyama@users.noreply.github.com>
2022-09-13 23:43:05 -0700

    production build

--- client/build/static/js/searchBar.js
+++
@@ -1,9 +0,0 @@
-// alert("serach bar acitve")
-$(document).ready(function(){
-    $(".live-search-box").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $(".live-search-list").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
-  });
